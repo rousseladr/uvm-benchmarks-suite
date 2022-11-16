d25a181efabac4ff107feef96973ee9a985660e9
Adrien Roussel <adrien.roussel@cea.fr>
2022-11-16 17:22:34 +0100

    Results plot with python script

---
+++ 01-numa_memcpy-managed/plot.py
@@ -0,0 +1,44 @@
+# doc : https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
+# doc : https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('a100_explicit.csv', delimiter='\t')
+
+y = df["gpu"]
+x = df["core"]
+A = np.zeros((x.max()+1,y.max()+1))
+A[x,y] = df["HostToDevice"]
+
+print(A.min())
+print(A.max())
+
+sns.heatmap(A, vmin=105, vmax=137, center=121, cmap="ocean")
+plt.title("Throughput (GB/s) of cudaMemcpy on A100 - Explicit")
+plt.suptitle("Host To Device")
+plt.xlabel('GPU Number')
+plt.ylabel('Core Number')
+plt.savefig("A100_explicit.pdf", format="pdf", bbox_inches="tight")
+#  plt.show()
+
+plt.clf()
+
+df = pd.read_csv('a100_implicit.csv', delimiter='\t')
+
+y = df["gpu"]
+x = df["core"]
+A = np.zeros((x.max()+1,y.max()+1))
+A[x,y] = df["HostToDevice"]
+
+print(A.min())
+print(A.max())
+
+sns.heatmap(A, vmin=105, vmax=137, center=121, cmap="ocean")
+plt.title("Throughput (GB/s) of cudaMemcpy on A100 - Implicit (Managed)")
+plt.suptitle("Host To Device")
+plt.xlabel('GPU Number')
+plt.ylabel('Core Number')
+plt.savefig("A100_implicit.pdf", format="pdf", bbox_inches="tight")
